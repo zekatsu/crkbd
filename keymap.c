a69b380740b82cdc568eae363b80c35bf8a6ee0c
zekatsu <katsuyoso@yahoo.co.jp>
2020-05-28 17:54:51 +0900

    a

--- keymap.c
+++ keymap.c
@@ -29,9 +29,9 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT( \
   //,---------------------ss-------------------------------.                    ,-----------------------------------------------------.
-       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,\
+       KC_ESC,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-  LGUI_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_MINS, KC_BSPC,\
+  LGUI_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, JP_COLN, KC_MINS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
        KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -103,9 +103,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
          KC_0,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,  JP_GRV, KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      JP_TILD,  JP_EQL, JP_PLUS, JP_ASTR,   JP_AT, JP_CIRC,                      JP_LCBR, JP_LBRC, JP_LPRN, JP_QUOT, JP_SCLN, KC_INT1,\
+      JP_TILD,  JP_EQL, JP_PLUS, JP_ASTR,   JP_AT, JP_CIRC,                      JP_LCBR, JP_LBRC, JP_LPRN, JP_QUOT, JP_SCLN, JP_UNDS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,   KC_LT,   KC_GT, JP_COMM,  JP_DOT, JP_PIPE,                      JP_RCBR, JP_RBRC, JP_RPRN,  JP_DQT, KC_INT3, KC_RSFT,\
+      KC_LSFT,   KC_LT,   KC_GT, JP_COMM,  JP_DOT, JP_PIPE,                      JP_RCBR, JP_RBRC, JP_RPRN,  JP_DQT, JP_BSLS, KC_RSFT,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX,  KC_SPC,   LOWER,      RAISE,  KC_ENT, XXXXXXX \
                                       //`--------------------------'  `--------------------------'
